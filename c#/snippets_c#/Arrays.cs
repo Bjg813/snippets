47364b6573b74e9cc0230664c928bc9dc5491de3
Brennangrout <bjg813@gmail.com>
2017-11-25 16:36:51 -0800

    mod array docs & new file Arrays.cs

---
+++ c#/snippets_c#/Arrays.cs
@@ -0,0 +1,20 @@
+/* 
+Introduction 
+*/
+
+double[] items1 = new double[10]; //Instantiate items1 array with "new"
+
+items1[0] = "sunshine"; //Setting "sunshine" to index 0
+
+string[] favoriteThings = new string[3]{"sunshine", "lollipops", "rainbows"}; //Allowing only 3 items to be indexed in the array and also setting the indexes
+
+string[] favoriteThings = {"sunshine", "lollipops", "rainbows"}; //Shorter method 
+
+favoriteThings.Length //Will give how many intems are in array "3"
+
+double myFavorite = favoriteThings[2]; //Setting a double variable to a favoriteThings array index 2 "lollipops"
+
+
+
+
+
